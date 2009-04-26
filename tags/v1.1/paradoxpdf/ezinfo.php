e943b9720a82e68cc0078c202e9bf3e163d2ee73
paradox <info@example.com>
2009-04-26 22:04:54 +0000

    update ezinfo version
    git-svn-id: http://svn.projects.ez.no/paradoxpdf@27 623c2771-4852-4cd3-b625-457504777409

--- tags/v1.1/paradoxpdf/ezinfo.php
+++ tags/v1.1/paradoxpdf/ezinfo.php
@@ -4,7 +4,7 @@ class paradoxpdfInfo
     function info()
     {
         return array( 'Name' => "ParadoxPDF",
-                      'Version' => "Beta_1.O",
+                      'Version' => "1.1",
                       'Copyright' => "Copyright (C) 2009 Mohamed Karnichi",
                       'License' => "GNU General Public License v2.0"
                      );
